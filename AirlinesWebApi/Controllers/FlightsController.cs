9c6ee9e4f78bf1391cb1dfabe97d5563b66251d0
Marco Baruffa <info.marcobaruffa@gmail.com>
2016-04-19 18:37:19 +0200

    added new layer wep api

---
+++ AirlinesWebApi/Controllers/FlightsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AirlinesWebApi.Controllers
+{
+    public class FlightsController : ApiController
+    {
+        public int Get()
+        {
+            return 42;
+        }
+    }
+}
